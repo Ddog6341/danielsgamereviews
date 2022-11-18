a0092440a3ccdfafc6e3b35b7556e9c13df227bd
DanielWheeler <danielwheeler6@yahoo.com>
2022-11-18 13:33:52 -0800

    added games to index.html

---
+++ main.js
@@ -0,0 +1,47 @@
+const gamestable = [ 
+    { 
+        name: 'The last of us Part 1',
+        rating: 10,
+    },
+    { 
+        name: 'God of War (2018)',
+        rating: 10,
+    },
+    {
+        name: 'Elden Ring',
+        rating: 10,
+    },
+    { 
+        name: 'The Last of Us part 2',
+        rating: 9,
+
+    }, 
+    {
+        name: 'Uncharted 4',
+        rating: 9,
+    },
+    {
+        name: 'Returnal',
+        rating: 9,
+    },
+    {
+        name: 'Marvels Spiderman',
+        rating: 9,
+    },
+    {
+        name: 'Farcry 3',
+        rating: 8,
+    },
+    {
+        name: 'Red Dead Redemption 2',
+        rating: 10,
+    },
+    {
+        name: 'For Honor',
+        rating: 7,
+    },
+    {
+        name: 'Stray',
+        rating: 8,
+    },
+];
