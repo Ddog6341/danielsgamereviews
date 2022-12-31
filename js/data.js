ba64364acacf3ae8e5b030f74659009ead3dcc0e
Daniel Wheeler <Danielwheeler6@yahoo.com>
2022-12-30 21:09:11 -0800

    changed game html and js

--- js/data.js
+++ js/data.js
@@ -4,57 +4,86 @@ export const gamesTable = [
         id: 1,
         name: 'The last of us Part 1',
         rating: 10,
+        recs: [1,2,3],
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt aliquam eros eget semper. Integer ac felis nibh. Vestibulum ligula tellus, ultrices at felis volutpat, ullamcorper lacinia augue.'
     },
     {
         id: 2,
         name: 'God of War (2018)',
         rating: 10,
+        recs: [1,2,3],
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt aliquam eros eget semper. Integer ac felis nibh. Vestibulum ligula tellus, ultrices at felis volutpat, ullamcorper lacinia augue.'
     },
     {
         id: 3,
         name: 'Elden Ring',
         rating: 10,
+        recs: [1,2,3],
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt aliquam eros eget semper. Integer ac felis nibh. Vestibulum ligula tellus, ultrices at felis volutpat, ullamcorper lacinia augue.'
     },
     {
         id: 4,
         name: 'The Last of Us part 2',
         rating: 9,
-
+        recs: [1,2,3],
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt aliquam eros eget semper. Integer ac felis nibh. Vestibulum ligula tellus, ultrices at felis volutpat, ullamcorper lacinia augue.'
+    
     }, 
     {
         id: 5,
         name: 'Uncharted 4',
         rating: 9,
+        recs: [1,2,3],
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt aliquam eros eget semper. Integer ac felis nibh. Vestibulum ligula tellus, ultrices at felis volutpat, ullamcorper lacinia augue.'
+    
     },
     {
         id: 6,
         name: 'Returnal',
         rating: 9,
+        recs: [1,2,3],
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt aliquam eros eget semper. Integer ac felis nibh. Vestibulum ligula tellus, ultrices at felis volutpat, ullamcorper lacinia augue.'
+    
     },
     {
         id: 7,
         name: 'Marvels Spiderman',
         rating: 9,
+        recs: [1,2,3],
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt aliquam eros eget semper. Integer ac felis nibh. Vestibulum ligula tellus, ultrices at felis volutpat, ullamcorper lacinia augue.'
+    
     },
     {
         id: 8,
         name: 'Farcry 3',
         rating: 8,
+        recs: [1,2,3],
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt aliquam eros eget semper. Integer ac felis nibh. Vestibulum ligula tellus, ultrices at felis volutpat, ullamcorper lacinia augue.'
+    
     },
     {
         id: 9,
         name: 'Red Dead Redemption 2',
         rating: 10,
+        recs: [1,2,3],
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt aliquam eros eget semper. Integer ac felis nibh. Vestibulum ligula tellus, ultrices at felis volutpat, ullamcorper lacinia augue.'
+    
     },
     {
         id: 10,
         name: 'For Honor',
         rating: 7,
+        recs: [1,2,3],
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt aliquam eros eget semper. Integer ac felis nibh. Vestibulum ligula tellus, ultrices at felis volutpat, ullamcorper lacinia augue.'
+    
     },
     {
         id: 11,
         name: 'Stray',
         rating: 8,
+        recs: [1,2,3],
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt aliquam eros eget semper. Integer ac felis nibh. Vestibulum ligula tellus, ultrices at felis volutpat, ullamcorper lacinia augue.'
+    
     },
 ];
 
--- js/game.js
+++ js/game.js
@@ -12,8 +12,39 @@ ready(function() {
     if (!game) {
         window.location.href = '../index.html';
     } else {
-        const titleElement = document.getElementById('title');
-        titleElement.innerText = game.name;
+        const gameContainerElement = document.getElementById('game-container');
+        
+        const gameHeaderElement = document.createElement("h2");
+        gameHeaderElement.innerText = game.name;
+        gameContainerElement.appendChild(gameHeaderElement);
+
+        const gameRatingElement = document.createElement("h2");
+        gameRatingElement.innerText = game.rating + "/10";
+        gameContainerElement.appendChild(gameRatingElement);
+
+        const gameReviewElement = document.createElement("p");
+        gameReviewElement.innerText = game.review;
+        gameContainerElement.appendChild(gameReviewElement);
+
+        const recHeaderElement= document.createElement("h2");
+        recHeaderElement.innerText = "recommended";
+        gameContainerElement.appendChild(recHeaderElement);
+
+        const recContainerElement = document.createElement("div");
+        gameContainerElement.appendChild(recContainerElement );
+
+        for (let recId of game.recs){
+            const rec = gamesTable.find(game => game.id === recId);
+            if(!rec) continue;
+            const recNameElement = document.createElement("h3");
+            recNameElement.innerText = rec.name;
+            recContainerElement.appendChild(recNameElement); 
+
+            const recRatingElement = document.createElement("span");
+            recRatingElement.innerText = rec.rating + "/10";
+            recContainerElement.appendChild(recRatingElement); 
+        }
+
     }
 });
 
