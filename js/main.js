f5c91f675ee52b4742cf9f90924c86be5666bb76
Justin Wheeler <justin.wheeler@gfs.com>
2023-01-18 12:04:10 -0500

    Created pagination logic with renderGamesPage in main.js

--- js/main.js
+++ js/main.js
@@ -6,10 +6,19 @@ function gotoGame(id){
     return () => window.location.href = 'html/game.html?id=' + id;
 }
 
-ready(function() {
-    const gamesContainerElement = document.getElementById('games-container');
+function renderGamesPage(page, size){
+    const curPage = page * size;
+    const nxtPage = (page + 1) * size;
+    const isLastPage = nxtPage > gamesTable.length;
+    const gamesPage = gamesTable.slice(
+        curPage, isLastPage ? gamesTable.length : nxtPage
+    );
 
-    for (let game of gamesTable){
+    // render games page
+    const gamesContainerElement = document.getElementById('games-container');
+    gamesContainerElement.innerHTML = ''; // clear children
+
+    for (let game of gamesPage){
         const gameElement = document.createElement('div');
         gameElement.classList.add('game');
         gameElement.addEventListener('click', gotoGame(game.id));
@@ -18,5 +27,22 @@ ready(function() {
         createTextElement('h2', game.name, gameElement);
         createTextElement('h2', formatRating(game.rating), gameElement);
     }
+
+    // render paging buttons
+    if(page > 0){
+        const prevButton = createTextElement('button', 'prev', gamesContainerElement);
+        prevButton.addEventListener('click', () => renderGamesPage(page - 1, size));
+    }
+
+    createTextElement('h3', `page ${page + 1}`, gamesContainerElement);
+
+    if(!isLastPage) {
+        const nextButton = createTextElement('button', 'next', gamesContainerElement);
+        nextButton.addEventListener('click', () => renderGamesPage(page + 1, size));
+    }
+}
+
+ready(function() {
+    renderGamesPage(0, 5);
 });
 
